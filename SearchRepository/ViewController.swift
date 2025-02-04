24a40ccafa043b65f3515a21172550896e4e27dd
Goban <gobanest@gmail.com>
2025-02-04 22:22:11 +0900

    initial setting

---
+++ SearchRepository/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RepositorySearch
+//
+//  Created by Lee Myeonghwan on 2/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ SearchRepositoryUITests/SearchRepositoryUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RepositorySearchUITestsLaunchTests.swift
+//  RepositorySearchUITests
+//
+//  Created by Lee Myeonghwan on 2/4/25.
+//
+
+import XCTest
+
+final class SearchRepositoryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
